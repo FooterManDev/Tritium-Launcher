552b38e3664e7567a7668a04e070103a32908293
FooterManDev <footerman.work@gmail.com>
2025-01-19 10:42:22 -0500

    Add root functions for getting files from various important directories.

--- src/desktopMain/kotlin/Extensions.kt
+++ src/desktopMain/kotlin/Extensions.kt
@@ -1,3 +1,4 @@
+import java.io.File
 import java.net.URI
 import java.net.URL
 
@@ -8,3 +9,25 @@ fun String.toUrl(): URL {
 fun String.toURI(): URI {
     return URI(this)
 }
+
+/**
+ * Returns File from ~/
+ */
+fun fromHome(child: String): File {
+    return File(System.getProperty("user.home"), child)
+}
+
+/**
+ * Returns File from ~/.tritium
+ */
+fun fromTR(child: String): File {
+    return File(fromHome(".tritium"), child)
+}
+
+/**
+ * Returns the user home directory
+ */
+fun userHome(): String {
+    return System.getProperty("user.home")
+}
+val userHome = userHome()
