54b4259c65f5067f05b4c05010e6f16832c5f9c1
FooterManDev <footerman.work@gmail.com>
2025-01-19 09:41:54 -0500

    State Management

---
+++ src/desktopMain/kotlin/model/serializers/DimensionSerializer.kt
@@ -0,0 +1,25 @@
+package model.serializers
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.awt.Dimension
+
+/**
+ * Custom serializer for [Dimension]
+ */
+class DimensionSerializer : KSerializer<Dimension> {
+    override val descriptor = PrimitiveSerialDescriptor("Dimension", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): Dimension {
+        val (width, height) = decoder.decodeString().split(",").map { it.toInt() }
+        return Dimension(width, height)
+    }
+
+    override fun serialize(encoder: Encoder, value: Dimension) {
+        encoder.encodeString("${value.width},${value.height}")
+    }
+
+}
